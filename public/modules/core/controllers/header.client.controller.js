b1eabdda8d86b08fab5c74c853c096c7b2873f4c
bnguyen86 <bnguyen@outlook.com>
2015-04-01 00:28:19 -0700

    Start new nav menu

--- public/modules/core/controllers/header.client.controller.js
+++ public/modules/core/controllers/header.client.controller.js
@@ -1,10 +1,25 @@
 'use strict';
 
-angular.module('core').controller('HeaderController', ['$scope', 'Authentication', 'Menus',
-	function($scope, Authentication, Menus) {
+angular.module('core').controller('HeaderController', ['$scope', 'Authentication', 'Menus', '$mdSidenav',
+	function($scope, Authentication, Menus, $mdSidenav) {
 		$scope.authentication = Authentication;
 		$scope.isCollapsed = false;
 		$scope.menu = Menus.getMenu('topbar');
+
+		$scope.toggleLeft = function() {
+		    $mdSidenav('left').toggle()
+		        .then(function() {
+		            //$log.debug("toggle left is done");
+       		});
+		};
+
+		$scope.close = function() {
+			$mdSidenav('left').close()
+				.then(function(){
+				//$log.debug("close LEFT is done");
+				});
+		};
+
 
 		$scope.toggleCollapsibleMenu = function() {
 			$scope.isCollapsed = !$scope.isCollapsed;
@@ -16,3 +31,7 @@ angular.module('core').controller('HeaderController', ['$scope', 'Authentication
 		});
 	}
 ]);
+
+angular.module('core').controller('LeftCtrl', function($scope, $timeout, $mdSidenav, $log) {
+	
+});
